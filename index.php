66191ed04c85ab35be3bef2729b3b15569090f1e
zlb323 <zach1bellew@gmail.com>
2017-11-29 20:48:00 -0700

    ttse

--- index.php
+++ index.php
@@ -4,15 +4,18 @@ require_once 'firebaseLib.php';
 // --- This is your Firebase URL
 $url = 'https://com.example.zach1.deadbolt/';
 // --- Use your token from Firebase here
-$token = '';
+$token = 'z6r78MuaafvpmcqAmEpzTQ0VI7kbYRV7mdEJgShz';
 // --- Here is your parameter from the http GET
 $arduino_data = $_GET['arduino_data'];
-echo $_GET['arduino_data'];
+echo $arduino_data;
+echo "t";
 // --- $arduino_data_post = $_POST['name'];
 // --- Set up your Firebase url structure here
 $firebasePath = '/';
 /// --- Making calls
 $fb = new fireBase($url, $token);
-$response = $fb->push($firebasePath, $arduino_data);
+//$response = $fb->push($firebasePath, $arduino_data);
+echo $response;
+echo "testst";
 sleep(2);
 ?>
