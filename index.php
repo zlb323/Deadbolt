3aac8165f5cd82a9e250c96df4c2e387b8f12e30
zlb323 <zach1bellew@gmail.com>
2017-11-29 20:24:59 -0700

    push to firebase testing

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-echo "test"
+echo "testing";
+require_once 'firebaseLib.php';
+// --- This is your Firebase URL
+$url = 'https://com.example.zach1.deadbolt/';
+// --- Use your token from Firebase here
+$token = '';
+// --- Here is your parameter from the http GET
+$arduino_data = $_GET['arduino_data'];
+echo $_GET['arduino_data'];
+// --- $arduino_data_post = $_POST['name'];
+// --- Set up your Firebase url structure here
+$firebasePath = '/';
+/// --- Making calls
+$fb = new fireBase($url, $token);
+$response = $fb->push($firebasePath, $arduino_data);
+sleep(2);
 ?>
